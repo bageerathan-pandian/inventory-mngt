d1507b90322749dd3e30e3b868599842757c9caa
bageerathan <bageerathan@rcktechiees.com>
2020-03-02 18:25:06 +0530

    updated

--- cron-job/cron-account-expiry.js
+++ cron-job/cron-account-expiry.js
@@ -20,23 +20,31 @@ const ProductPaymentModel = require('../models/product_payment.model');
 //     });
 
     // do expired account. it runs every day
-var task3 = nodeCron.schedule("0 0 * * *", () => {
+var task3 = nodeCron.schedule("* * * * *", () => {
     console.log("will run every day at 12:00 AM");
     ProductPaymentModel.find({expiry_date:  {$lte: moment().format(), $ne: null }}, (err, result) => {
         if (err) return next(err);
         // console.log(result);
-        console.log(result.length);
+        console.log('length',result.length);
         // return result
         for(let data of result){            
             let updata = {status: 2}
-            console.log(data.company_details_id);
-            console.log(updata);
-            UserModel.update({company_details_id:data.company_details_id}, updata,{new: true}, (err, result1) => {
+            // console.log(data.company_details_id);
+            UserModel.find({company_details_id:data.company_details_id},(err, result1) => {
                 if (err) return next(err);
-                console.log(result1);
+                console.log('userdata',result1);                
+                for(let data1 of result1){  
+                    if(data1.role != 0){
+                        UserModel.update({company_details_id:data.company_details_id}, updata,{new: true}, (err, result2) => {
+                            if (err) return next(err);
+                            console.log(result2);
+                        });
+                    }
+                }
             });
         }
-    });
+    
+})
   });
 
   
